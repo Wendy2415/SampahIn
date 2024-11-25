138b5a494fa552a913c257c82d0fee1f669c59be
Wendy2415 <150636679+Wendy2415@users.noreply.github.com>
2024-11-25 13:03:52 +0700

    first commit

--- app/src/main/java/com/capstone/sampahin/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/capstone/sampahin/ui/chat/ChatViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ChatViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is chatFragment"
     }
     val text: LiveData<String> = _text
 }
